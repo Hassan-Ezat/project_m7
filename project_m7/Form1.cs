ffb3d0cd1317a20cd18c94eaa669cf5f8c087e88
hassan <ezath1226@gmail.com>
2025-04-18 11:17:04 +0200

    login design screen

--- project_m7/Form1.cs
+++ project_m7/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace project_m7
@@ -15,6 +10,76 @@ namespace project_m7
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("تم الضغط على الزر");
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            int radius = 50;
+            Graphics g = e.Graphics;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            using (GraphicsPath path = new GraphicsPath())
+            {
+                Rectangle bounds = ((Panel)sender).ClientRectangle;
+                path.AddArc(0, 0, radius, radius, 180, 90);
+                path.AddArc(bounds.Width - radius, 0, radius, radius, 270, 90);
+                path.AddArc(bounds.Width - radius, bounds.Height - radius, radius, radius, 0, 90);
+                path.AddArc(0, bounds.Height - radius, radius, radius, 90, 90);
+                path.CloseFigure();
+                using (Pen pen = new Pen(Color.Gray, 2))
+                {
+                    g.DrawPath(pen, path);
+                }
+            }
+        }
+
+        private void SetRoundedRegion(Control control, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            path.AddArc(0, 0, radius, radius, 180, 90);
+            path.AddArc(control.Width - radius, 0, radius, radius, 270, 90);
+            path.AddArc(control.Width - radius, control.Height - radius, radius, radius, 0, 90);
+            path.AddArc(0, control.Height - radius, radius, radius, 90, 90);
+            path.CloseAllFigures();
+            control.Region = new Region(path);
+        }
+
+        private void guna2TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
